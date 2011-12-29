7b4617b4a6dd1f05aa475188ed0d1522827b8e0e
cartoixa <cartoixa@localhost>
2011-12-29 18:12:05 +0100

    Forgot some files in the las commit.

---
+++ src/Library/Ogc/WebCatalog/Cql/Ast/DateTimeLiteralNode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Irony.Interpreter;
+using Irony.Interpreter.Ast;
+using Irony.Parsing;
+
+namespace OgcToolkit.Ogc.WebCatalog.Cql.Ast
+{
+
+#pragma warning disable 3001, 3009
+    public class DateTimeLiteralNode:
+        AstNode
+    {
+
+        public override void Init(ParsingContext context, ParseTreeNode treeNode)
+        {
+            base.Init(context, treeNode);
+
+            _Value=XmlConvert.ToDateTime(treeNode.Token.ValueString, XmlDateTimeSerializationMode.RoundtripKind);
+            AsString=string.Format(
+                CultureInfo.InvariantCulture,
+                "{0:G} (date time)",
+                _Value
+            );
+        }
+
+        protected override object DoEvaluate(ScriptThread thread)
+        {
+            return _Value;
+        }
+
+        public override bool IsConstant()
+        {
+            return true;
+        }
+
+        public DateTime Value
+        {
+            get
+            {
+                return _Value;
+            }
+        }
+
+        private DateTime _Value;
+    }
+#pragma warning restore 3001, 3009
+}
---
+++ src/Library/Ogc/WebCatalog/Cql/Ast/ExpressionBuilderUtils.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using Irony.Interpreter.Ast;
+
+namespace OgcToolkit.Ogc.WebCatalog.Cql.Ast
+{
+
+    internal static class ExpressionBuilderUtils
+    {
+
+        public static Expression CreateComparisonExpression(ExpressionBuilderParameters parameters, ExpressionType exType, AttributeNameNode left, object right)
+        {
+            Type subtype=left.GetExpressionStaticType(parameters);
+
+            // Custom implementation
+            if (parameters.OperatorImplementationProvider!=null)
+            {
+                object[] pa=new object[] { null, right };
+
+                object instance;
+                MethodInfo method=parameters.OperatorImplementationProvider.GetImplementation(exType.ToString(), new Type[] { subtype, subtype }, ref pa, out instance);
+                if (method!=null)
+                {
+                    Debug.Assert(pa.Length==2);
+                    Type st=(pa[1]!=null ? pa[1].GetType() : subtype); // Parameter types may have been changed
+
+                    Expression op=null;
+                    if (instance!=null)
+                        op=Expression.Call(
+                            Expression.Constant(instance),
+                            method,
+                            left.CreateExpression(parameters, st),
+                            Expression.Constant(pa[1], st)
+                        );
+                    else
+                        op=Expression.Call(
+                            method,
+                            left.CreateExpression(parameters, st),
+                            Expression.Constant(pa[1], st)
+                        );
+
+
+                    Type rt=Nullable.GetUnderlyingType(method.ReturnType)??method.ReturnType;
+                    if (method.ReturnType==typeof(bool))
+                        return Expression.IsTrue(op);
+                    else
+                        return Expression.Equal(
+                            op,
+                            Expression.Constant(Convert.ChangeType(true, rt), method.ReturnType)
+                        );
+                }
+            }
+
+            // Standard implementation
+            return Expression.MakeBinary(
+                exType,
+                left.CreateExpression(parameters, subtype),
+                Expression.Constant(right, subtype)
+            );
+        }
+    }
+}
