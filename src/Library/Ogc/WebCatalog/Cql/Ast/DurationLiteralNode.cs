087b06e5560e4bdd2e75298c54daa07796f5e2a6
Mac <mcartoixa@users.noreply.github.com>
2011-12-29 18:12:05 +0100

    Forgot some files in the las commit.

---
+++ src/Library/Ogc/WebCatalog/Cql/Ast/DurationLiteralNode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Irony.Interpreter;
+using Irony.Interpreter.Ast;
+using Irony.Parsing;
+
+namespace OgcToolkit.Ogc.WebCatalog.Cql.Ast
+{
+
+#pragma warning disable 3001, 3009
+    public class DurationLiteralNode:
+        AstNode
+    {
+
+        public override void Init(ParsingContext context, ParseTreeNode treeNode)
+        {
+            base.Init(context, treeNode);
+
+            _Value=XmlConvert.ToTimeSpan(treeNode.Token.ValueString);
+            AsString=string.Format(
+                CultureInfo.InvariantCulture,
+                "{0:G} (duration)",
+                _Value
+            );
+        }
+
+        protected override object DoEvaluate(ScriptThread thread)
+        {
+            return _Value;
+        }
+
+        public override bool IsConstant()
+        {
+            return true;
+        }
+
+        public TimeSpan Value
+        {
+            get
+            {
+                return _Value;
+            }
+        }
+
+        private TimeSpan _Value;
+    }
+#pragma warning restore 3001, 3009
+}
---
+++ src/Library/Ogc/WebCatalog/Cql/Ast/IExpressionBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace OgcToolkit.Ogc.WebCatalog.Cql.Ast
+{
+
+    public interface IExpressionBuilder
+    {
+
+        Expression CreateExpression(ExpressionBuilderParameters parameters, Type expectedStaticType);
+        Type GetExpressionStaticType(ExpressionBuilderParameters parameters);
+    }
+}
