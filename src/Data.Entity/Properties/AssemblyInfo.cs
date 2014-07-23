8bf5a37e0774fea32387aa54c07fb637a288fbfd
Mac <mcartoixa@users.noreply.github.com>
2014-07-23 21:29:02 +0200

    Fix the build

---
+++ src/Data.Entity/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+// This file is part of GeoSIK.
+// Copyright (C) 2012 Isogeo
+//
+// GeoSIK is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// GeoSIK is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with GeoSIK. If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("GeoSik.Data.Entity")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("d1b4ec47-55ec-4f25-8d01-c2109fe1345e")]
+
+[assembly: NeutralResourcesLanguage("en-US")]
