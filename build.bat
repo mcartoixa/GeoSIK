e81ea2b74b0b561470b28a4efd135978518c47cc
Mathieu Cartoixa <mathieu.cartoixa@isogeo.fr>
2016-05-27 14:40:53 +0200

    Fix AppVeyor build

--- build.bat
+++ build.bat
@@ -35,7 +35,7 @@ GOTO ARGS
 :: Builds the project
 :: -------------------------------------------------------------------
 :BUILD
-.nuget\NuGet.exe install ".nuget\packages.config" -o packages -source "https://api.nuget.org/v3/index.json" -source "http://CHANTIERS.hq.isogeo.fr/nuget/nuget/" -source "http://NUGET.hq.isogeo.fr/nuget/" -source "%LocalAppData%\NuGet\Cache"
+.nuget\NuGet.exe install ".nuget\packages.config" -o packages -source "https://api.nuget.org/v3/index.json" -source "http://CHANTIERS.hq.isogeo.fr/nuget/nuget/" -source "%LocalAppData%\NuGet\Cache"
 msbuild.exe %PROJECT% /nologo /t:%TARGET% /m:%NUMBER_OF_PROCESSORS% /p:GenerateDocumentation="%GENERATE_DOCUMENTATION%" /fl /flp:logfile=build.log;verbosity=%VERBOSITY%;encoding=UTF-8 /nr:False
 
 IF ERRORLEVEL 1 (
