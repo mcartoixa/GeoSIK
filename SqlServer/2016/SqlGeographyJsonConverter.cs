7b89a1d809ba33615d287a684b307792a91b0786
Mathieu Cartoixa <mathieu.cartoixa@free.fr>
2016-11-03 18:05:41 +0100

    Add Sql Server 2016 library

---
+++ SqlServer/2016/SqlGeographyJsonConverter.cs
@@ -0,0 +1,67 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+// This file is part of GeoSIK.
+// Copyright (C) 2012 Isogeo
+//
+// GeoSIK is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// GeoSIK is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with GeoSIK. If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using Newtonsoft.Json;
+using SqlTypes = Microsoft.SqlServer.Types;
+
+namespace GeoSik.SqlServer
+{
+
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    ///
+    /// <summary>Converts a <see cref="SqlGeography" /> to JSON.</summary>
+    /// <remarks>
+    ///   <para>The returned JSON is in the <see href="http://en.wikipedia.org/wiki/World_Geodetic_System">WGS84</see>
+    /// coordinate reference system.</para>
+    /// </remarks>
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+
+    public class SqlGeographyJsonConverter:
+        Ogc.SimpleFeature.GeometryJsonConverter
+    {
+
+        /// <summary>Writes the GeoJSON representation of the object.</summary>
+        /// <param name="writer">The <see cref="JsonWriter" /> to write to.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var g = value as SqlGeography;
+            if ((g!=null) && ((SqlTypes.SqlGeography)g).InstanceOf("FULLGLOBE"))
+            {
+                // ?
+                writer.WriteNull();
+                return;
+            } else
+                base.WriteJson(writer, value, serializer);
+        }
+
+        /// <summary>Creates the <see cref="IGeometryBuilder" /> that will be used to deserialize the
+        /// JSON input into a <see cref="SqlGeography" /> instance.</summary>
+        /// <returns>A <see cref="SqlGeometryBuilder" />.</returns>
+        protected override IGeometryBuilder CreateGeometryBuilder()
+        {
+            return new SqlGeometryBuilder();
+        }
+    }
+}
---
+++ SqlServer/2016/SqlGeometryBuilderWrapper.cs
@@ -0,0 +1,105 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+// This file is part of GeoSIK.
+// Copyright (C) 2012 Isogeo
+//
+// GeoSIK is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// GeoSIK is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with GeoSIK. If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using SqlTypes=Microsoft.SqlServer.Types;
+
+namespace GeoSik.SqlServer
+{
+
+    internal sealed class SqlGeometryBuilderWrapper:
+        IGeometryBuilder
+    {
+
+        public SqlGeometryBuilderWrapper()
+        {
+            _Builder=new SqlTypes.SqlGeometryBuilder();
+        }
+
+        public void SetCoordinateSystem(ICoordinateSystem system)
+        {
+            _Builder.SetSrid(system.Code);
+            _TargetSystem=system;
+        }
+
+        public void BeginGeometry(GeometryType type)
+        {
+            _Builder.BeginGeometry(GeometryTypeUtils.ConvertToGeometry(type));
+        }
+
+        public void BeginFigure(double x, double y, double? z)
+        {
+            _Builder.BeginFigure(x, y, z, null);
+        }
+
+        public void AddLine(double x, double y, double? z)
+        {
+            _Builder.AddLine(x, y, z, null);
+        }
+
+        public void EndFigure()
+        {
+            _Builder.EndFigure();
+        }
+
+        public void EndGeometry()
+        {
+            _Builder.EndGeometry();
+        }
+
+        public void Parse(string text, ICoordinateSystem system)
+        {
+            _Geometry=new SqlGeometry(SqlTypes.SqlGeometry.STGeomFromText((SqlChars)((SqlString)text), system.Code), system);
+        }
+
+        public void Parse(byte[] data, ICoordinateSystem system)
+        {
+            _Geometry=new SqlGeometry(SqlTypes.SqlGeometry.STGeomFromWKB(new SqlBytes(data), system.Code), system);
+        }
+
+        public SqlGeometry ConstructedGeometry
+        {
+            get
+            {
+                if (_Geometry==null)
+                    _Geometry=new SqlGeometry(_Builder.ConstructedGeometry, _TargetSystem);
+
+                return _Geometry;
+            }
+        }
+
+        ISimpleGeometry IGeometryBuilder.ConstructedGeometry
+        {
+            get
+            {
+                return ConstructedGeometry;
+            }
+        }
+
+        private SqlTypes.SqlGeometryBuilder _Builder;
+        private SqlGeometry _Geometry;
+        private ICoordinateSystem _TargetSystem;
+    }
+}
