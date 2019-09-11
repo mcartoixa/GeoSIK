a2bcbce6a3508fccf87074da5361b3a56f220515
Vianney Doleans <vianney.doleans@epitech.eu>
2019-09-11 12:05:17 +0200

    Add versionning nuget packages

--- Library/Properties/AssemblyInfo.cs
+++ Library/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Library")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library that allows the use OGC standards in .NET applications.")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
